ed8139fb38464f8906abc38b111c030c6622a9c3
CHINTAKYALA BHARGAVI <bhargavichintakayala07@gmail.com>
2025-12-13 22:17:57 +0530

    Remove LoadingDots component implementation

--- LoadingDots.tsx
+++ LoadingDots.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
 
-export const LoadingDots = () => {
-  return (
-    <div className="flex space-x-1 items-center p-2">
-      <div className="w-2 h-2 bg-teal-500 rounded-full animate-bounce" style={{ animationDelay: '0s' }}></div>
-      <div className="w-2 h-2 bg-teal-500 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
-      <div className="w-2 h-2 bg-teal-500 rounded-full animate-bounce" style={{ animationDelay: '0.4s' }}></div>
-    </div>
-  );
-};
